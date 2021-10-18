e0e2f22e806fa10bf1f87ea137bbbbae54e9d0f2
ZeynepZincirli <zeynep.zincirli@ing.com.tr>
2021-10-18 19:28:24 +0300

    LoginParkingApp

---
+++ ParkingApp/ViewControllers/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  LoginWithFullPicture
+//
+//  Created by PCMACEGITIM on 16.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var girisButton: UIButton!
+    @IBOutlet weak var cardView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // cardView
+        cardView.layer.cornerRadius = 10.0
+        cardView.layer.shadowOpacity = 0.5
+        cardView.layer.shadowRadius = 10
+        cardView.layer.shadowOffset = CGSize (width: 10, height: 10)
+        // girisButton
+        girisButton.layer.cornerRadius = 10.0
+    }
+
+
+}
+
