2be8b88e45ffdb751b4c93a55531e1537490a122
Burak <omerburakvural@gmail.com>
2021-10-22 11:21:43 +0300

    main map screen location services added

--- ParkingApp/ViewControllers/MainMapViewController.swift
+++ ParkingApp/ViewControllers/MainMapViewController.swift
@@ -6,16 +6,73 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MainMapViewController: UIViewController {
+class MainMapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    let regionRadius: Double = 2000.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        checkLocationServices()
+    }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func checkLocationServices(){
+        if CLLocationManager.locationServicesEnabled(){
+            setupLocationManager()
+            checkLocationAuth()
+        }else{
+            // show alert to user for enabling location services
+        }
+    }
+    
+    func centerViewUserLocation(){
+        if let location = locationManager.location?.coordinate{
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func checkLocationAuth(){
+        switch CLLocationManager.authorizationStatus(){
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            break
+        case .denied:
+            break
+        case .authorizedAlways:
+            break
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            centerViewUserLocation()
+        @unknown default:
+            break
+        }
     }
 
     @IBAction func backButtonClicked(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
+}
+
+extension MapDetailViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        //
+    }
 }
