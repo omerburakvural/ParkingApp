52de20e31e91b54febc559faf22cbca2888b02cb
Burak <omerburakvural@gmail.com>
2021-10-26 19:06:25 +0300

    pins from database showing on the map

--- ParkingApp/ViewControllers/MainMapViewController.swift
+++ ParkingApp/ViewControllers/MainMapViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 import MapKit
 import CoreLocation
 import MaterialComponents
+import FirebaseDatabase
+import FirebaseFirestore
+import Firebase
+import FirebaseAnalytics
+
 
 class MainMapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
@@ -25,42 +30,49 @@ class MainMapViewController: UIViewController, MKMapViewDelegate, CLLocationMana
         super.viewDidLoad()
         mapView.delegate = self
         checkLocationServices()
-        addCustomPin()
+        addPinToTheMap()
+        manualPinAdd()
     }
     
-    func addCustomPin(){
-        let pin = MKPointAnnotation()
-        pin.coordinate = coordinate
-        pin.title = "Otopark"
-        mapView.addAnnotation(pin)
+    
+    
+    func manualPinAdd(){
+        let annotationsArray = [
+            ["name": "Özel Acıbadem Hastanesi Otoparkı","lat": 41.007812,"long": 29.042895],
+            ["name": "Akasya AVM Kapalı Otoparkı","lat": 41.004,"long": 29.0546],
+            ["name": "Kopuz Otopark","lat": 41.024303,"long": 29.045468],
+            ["name": "İspark Otopark","lat": 40.996772,"long": 29.022098],
+            ["name": "On Park Kaya Otopark","lat": 40.992152,"long": 29.025879],
+            ["name": "Özel Kuşdili Otopark","lat": 40.989621,"long": 29.032697]
+        ]
+        createManuelAnnotations(locations: annotationsArray)
     }
     
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        guard !(annotation is MKUserLocation) else{
-            return nil
+    func createManuelAnnotations(locations: [[String: Any]]){
+        for location in locations{
+            let annotations = MKPointAnnotation()
+            annotations.title = location["name"] as? String
+            annotations.coordinate = CLLocationCoordinate2D(latitude: location["lat"] as! CLLocationDegrees, longitude: location["long"] as! CLLocationDegrees)
+            mapView.addAnnotation(annotations)
         }
         
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "custom")
-        
-        if annotationView == nil {
-            //create view
-            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "custom")
-            annotationView?.canShowCallout = true
-            annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-        }else {
-            annotationView?.annotation = annotation
-        }
-        
-        annotationView?.image = UIImage(named: "mapPin")
-        return annotationView
     }
     
-    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-        let storyboard = UIStoryboard(name: "MapDetail", bundle: nil)
-        if let vc = storyboard.instantiateViewController(withIdentifier: "mapDetail") as? MapDetailViewController {
-            let bottomSheet = MDCBottomSheetController(contentViewController: vc)
-            self.present(bottomSheet, animated: true, completion: nil)
+    func addPinToTheMap(){
+        var pinArray: [MKPointAnnotation] = []
+        for i in 0...20 {
+            viewModel.loadPin(withIndex: i)
+            let pin = MKPointAnnotation()
+            pin.coordinate = CLLocationCoordinate2D(latitude: viewModel.getLat(), longitude: viewModel.getLong())
+            pin.title = viewModel.getName()
+            pinArray.append(pin)
         }
+        mapView.addAnnotations(pinArray)
+    }
+    
+    @IBAction func loadDataButton(_ sender: Any) {
+        addPinToTheMap()
+        print(viewModel.getName())
     }
     
     @IBAction func editProfileButtonClicked(_ sender: Any) {
@@ -69,11 +81,6 @@ class MainMapViewController: UIViewController, MKMapViewDelegate, CLLocationMana
             vc.modalPresentationStyle = .fullScreen
             self.present(vc, animated: false, completion: nil)
         }
-    }
-    
-    func setupLocationManager() {
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
     
     func checkLocationServices(){
@@ -90,22 +97,13 @@ class MainMapViewController: UIViewController, MKMapViewDelegate, CLLocationMana
         }
     }
     
-    func centerViewUserLocation(){
-        if let location = locationManager.location?.coordinate{
-            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: visibleRadiusForUser, longitudinalMeters: visibleRadiusForUser)
-            mapView.setRegion(region, animated: true)
-        }
-    }
-    
-    func centerGeneralArea(){
-        let location = CLLocationCoordinate2D(latitude: 41.013393, longitude: 29.047944)
-        let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
-        mapView.setRegion(region, animated: true)
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
     
     func checkLocationAuth(){
         switch CLLocationManager.authorizationStatus(){
-            
         case .notDetermined:
             locationManager.requestWhenInUseAuthorization()
         case .restricted:
@@ -129,6 +127,19 @@ class MainMapViewController: UIViewController, MKMapViewDelegate, CLLocationMana
         @unknown default:
             break
         }
+    }
+    
+    func centerViewUserLocation(){
+        if let location = locationManager.location?.coordinate{
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: visibleRadiusForUser, longitudinalMeters: visibleRadiusForUser)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func centerGeneralArea(){
+        let location = CLLocationCoordinate2D(latitude: 41.013393, longitude: 29.047944)
+        let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(region, animated: true)
     }
     
     func checkForRegion(){
@@ -164,4 +175,31 @@ class MainMapViewController: UIViewController, MKMapViewDelegate, CLLocationMana
         checkLocationAuth()
     }
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard !(annotation is MKUserLocation) else{
+            return nil
+        }
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "custom")
+        
+        if annotationView == nil {
+            //create view
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "custom")
+            annotationView?.canShowCallout = true
+            annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }else {
+            annotationView?.annotation = annotation
+        }
+        
+        annotationView?.image = UIImage(named: "mapPin")
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        let storyboard = UIStoryboard(name: "MapDetail", bundle: nil)
+        if let vc = storyboard.instantiateViewController(withIdentifier: "mapDetail") as? MapDetailViewController {
+            let bottomSheet = MDCBottomSheetController(contentViewController: vc)
+            self.present(bottomSheet, animated: true, completion: nil)
+        }
+    }
 }
--- ParkingApp/ViewModels/MainMapViewModel.swift
+++ ParkingApp/ViewModels/MainMapViewModel.swift
@@ -5,8 +5,86 @@
 //  Created by Omer Burak Vural on 13.10.2021.
 //
 
-import Foundation
+import UIKit
+import FirebaseDatabase
+import Firebase
+
+struct Pin{
+    var lat: Double
+    var long: Double
+    var name: String
+}
 
 class MainMapViewModel {
-     
+    
+    let ref = Database.database().reference(withPath: "Park")
+    var name = ""
+    var lat: Double = 0.0
+    var long: Double = 0.0
+    var numberOfParks = 0
+    
+    func loadPin(withIndex: Int) {
+        let index = String(withIndex)
+        let childRef = ref.child(index)
+        
+        childRef.child("lat").observeSingleEvent(of: .value, with: {(snapshot) in
+            self.setLat(withCoordinate: snapshot.value as! Double)
+        })
+        childRef.child("long").observeSingleEvent(of: .value, with: {(snapshot) in
+            self.setLong(withCoordinate: snapshot.value as! Double)
+        })
+        childRef.child("park_name").observeSingleEvent(of: .value, with: {(snapshot) in
+            self.setName(withName: snapshot.value as! String)
+        })
+        ref.observe(.value, with: { snapshot in
+            self.setNumberOfParks(withNumber: Int(snapshot.childrenCount))
+        })
+    }
+    
+    func setLat(withCoordinate: Double){
+        lat = withCoordinate
+    }
+    
+    func setLong(withCoordinate: Double){
+        long = withCoordinate
+    }
+    func setName(withName: String){
+        name = withName
+    }
+    
+    func setNumberOfParks(withNumber: Int){
+        numberOfParks = withNumber
+    }
+    
+    
+    func getName() -> String {
+        return name
+    }
+    
+    func getLat() -> Double {
+        return lat
+    }
+    
+    func getLong() -> Double {
+        return long
+    }
+    
+    func getNumberOfParks() -> Int {
+        return numberOfParks
+    }
 }
+
+//class ParkModel: Codable {
+//    var capacity: Int = 0
+//    var has_vale: String = ""
+//    var lat: Double = 0.0
+//    var long: Double = 0.0
+//    var lpg_availability: String = ""
+//    var park_name: String = ""
+//    var park_type: String = ""
+//    var payment_type: String = ""
+//    var price: Int = 0
+//}
+
+
+
