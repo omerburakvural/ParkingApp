9dfd2a7bca3efa4b694979d3bb3cb54c80facb8f
Adem Er <eradem34@gmail.com>
2021-10-28 22:26:41 +0300

    Harita Detay Sayfası tasarımı yapıldı.

--- ParkingApp/ViewControllers/MapDetailViewController.swift
+++ ParkingApp/ViewControllers/MapDetailViewController.swift
@@ -9,9 +9,35 @@ import UIKit
 
 class MapDetailViewController: UIViewController {
 
+    @IBOutlet weak var stackViewSolListe: UIStackView!
+    
+    @IBOutlet weak var stackViewSagVeriler: UIStackView!
+    
+    @IBOutlet weak var fiyatLabel: UILabel!
+    
+    
+    @IBOutlet weak var odemeKabulLabel: UILabel!
+    @IBOutlet weak var lpgkabulLabel: UILabel!
+    @IBOutlet weak var otoparkTipiLabel: UILabel!
+    @IBOutlet weak var kapasiteLabel: UILabel!
+    
+    @IBOutlet weak var rotaCizButon: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.layer.cornerRadius = 15.0
+        
+        if traitCollection.userInterfaceStyle == .dark
+        {
+            view.backgroundColor = .systemGray
+            rotaCizButon.backgroundColor = .systemGray
+            rotaCizButon.tintColor = .black
+//            kayitButon.backgroundColor = .systemGray
+        }
+        else
+        {
+            view.backgroundColor = UIColor(red: 4/255.0, green: 159/255.0, blue: 217/255.0, alpha: 1)
+        }
         // Do any additional setup after loading the view.
     }
 
